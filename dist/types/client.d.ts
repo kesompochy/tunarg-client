cbdf2d11ecd7cdff72c5063226e393ea3be14925
kesompochy <kesompochy@gmail.com>
2022-04-03 22:45:11 +0900

    _initWebSocket()を実装。再接続してもイベントリスナが継承されるように

--- dist/types/client.d.ts
+++ dist/types/client.d.ts
@@ -1,15 +1,21 @@
 export default abstract class Client {
     ws: WebSocket;
+    host: string | URL;
     private readonly _actions;
     get actions(): {
         [x: string]: Function;
         [x: number]: Function;
         [x: symbol]: Function;
     };
-    constructor(host: string, actions?: {
+    constructor(host: string | URL, actions?: {
         [K in any]: Function;
     });
+    private _connectWebSocket;
+    private _initWebSocket;
+    private _addMessageEvent;
+    private _addCloseEvent;
     send(type: string, content: any): void;
     addAction(type: string, act: Function): void;
+    doAction(type: string, content: any): void;
 }
 //# sourceMappingURL=client.d.ts.map
--- src/client.ts
+++ src/client.ts
@@ -2,36 +2,68 @@ import ITunargProtocol from "./protocol";
 
 export default abstract class Client{
     ws: WebSocket;
+    host: string | URL;
     private readonly _actions: {
         [K in any]: Function;
     } = {};
     get actions() {
         return this._actions;
     }
-    constructor(host: string, actions?: {[K in any]: Function}){
+    constructor(host: string | URL, actions?: {[K in any]: Function}){
         const ws = new WebSocket(host);
         this.ws = ws;
+        this.host = host;
 
         for(let type in actions){
             this.addAction(type, actions[type]);
         }
 
-        ws.addEventListener('message', (mes)=>{
-            const json: ITunargProtocol = JSON.parse(mes.data);
-
-            const type = json.type;
-            const content = json.content;
-            
-            this._actions[type](content);
-        })
+        this._initWebSocket(ws);
+    }
+    private _connectWebSocket(host: string | URL){
+        const ws = new WebSocket(host);
+        this.ws = ws;
+        this._initWebSocket(ws);
+    }
+    private _initWebSocket(ws: WebSocket){
+        this._addMessageEvent(ws);
+        this._addCloseEvent(ws);
+    }
+    private _addMessageEvent(ws: WebSocket){
+        ws.addEventListener('open', ()=>{
+            ws.addEventListener('message', (mes)=>{
+                const json: ITunargProtocol = JSON.parse(mes.data);
+    
+                const type = json.type;
+                const content = json.content;
+                
+                this.doAction(type, content);
+            });
+        });
+    }
+    private _addCloseEvent(ws: WebSocket){
+        ws.addEventListener('close', (e: Event)=>{
+            console.log('closed! i will connect again...');
+            this._connectWebSocket(this.host);
+        });
     }
     send(type: string, content: any){
-        this.ws.send(JSON.stringify({
-            type: type,
-            content: content
-        }));
+        const ws = this.ws;
+        if(ws.readyState == ws.OPEN){
+
+            ws.send(JSON.stringify({
+                type: type,
+                content: content
+            }));
+        }
     }
     addAction(type: string, act: Function){
         this._actions[type] = act;
+    }
+    doAction(type: string, content: any){
+        const ws = this.ws;
+        if(ws.readyState == ws.OPEN){
+            this._actions[type](content);
+        }
     }
 }
