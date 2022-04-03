85884406161ff23be45a3d22f630c0bd06ac7cd2
kesompochy <kesompochy@gmail.com>
2022-04-03 19:56:03 +0900

    send周りを整備

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.ts',
     mode: 'production',
+    devtool: 'source-map',
     output: {
         path: path.resolve(__dirname, 'dist'),
         library: {
